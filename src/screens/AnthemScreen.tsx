acead860ecaa69a5c4445aec60aacb2a4ebc3791
Ronis <valdirronis@gmail.com>
2024-02-17 00:09:27 -0300

    Rolar para o topo após selecionar novo hino

--- src/screens/AnthemScreen.tsx
+++ src/screens/AnthemScreen.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {View, ScrollView} from 'react-native';
-import {activateKeepAwake} from '@sayem314/react-native-keep-awake';
+import {
+  activateKeepAwake,
+  deactivateKeepAwake,
+} from '@sayem314/react-native-keep-awake';
 import AnthemsModal from '../components/modals/AnthemsModal';
 import IndexesModal from '../components/modals/IndexesModal';
 import FavoritesModal from '../components/modals/FavoritesModal';
@@ -10,15 +13,33 @@ import AnthemVerses from '../components/AnthemVerses';
 import AnthemAuthor from '../components/AnthemAuthor';
 import AnthemHeaderBar from '../components/AnthemHeaderBar';
 import AnthemAudioProgress from '../components/AnthemAudioProgress';
+import {useAppContext} from '../providers/AppProvider';
 import MenuButton from '../components/MenuButton';
 import {styles} from '../utils/theme';
 
 const AnthemScreen: React.FC = () => {
-  activateKeepAwake();
+  const scrollRef = React.useRef<ScrollView>(null);
+  const {state} = useAppContext();
+
+  React.useEffect(() => {
+    activateKeepAwake();
+    return () => {
+      deactivateKeepAwake();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (state.currentAnthem) {
+      scrollRef.current?.scrollTo({y: 0, animated: false});
+    }
+  }, [state.currentAnthem]);
 
   return (
     <View style={styles.container}>
-      <ScrollView stickyHeaderIndices={[0]} stickyHeaderHiddenOnScroll>
+      <ScrollView
+        stickyHeaderIndices={[0]}
+        stickyHeaderHiddenOnScroll
+        ref={scrollRef}>
         <AnthemHeaderBar />
         <AnthemTitle />
         <AnthemVerses />
